ba62d71dbbe99523a698695f9cfd508094d64f7f
Nikhil Bharadwaj <nikhiln64@gmail.com>
2017-12-06 19:50:40 -0500

    Csv_RandomData_Generator

---
+++ Csv_RandomData_Generator/src/GenerateData/CreateCsvData.java
@@ -0,0 +1,87 @@
+package GenerateData;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class CreateCsvData {
+	// Delimiter used in CSV file
+	private static final String COMMA_DELIMITER = ",";
+	private static final String NEW_LINE_SEPARATOR = "\n";
+
+	// CSV file header
+	private static final String FILE_HEADER = "TRADE_FLOW,MONTH,YEAR,HS_CODE,PRODUCT_NAME,TOTAL_WEIGHT,WEIGHT_UNIT,PRICE_PER_UNIT($),QUANTITY";
+
+	public static void writeCsvFile(String fileName, int numberOfRecords) {
+		GenerateData generateData = new GenerateData();
+		List<TradeDataVO> tradeDataVOList = new ArrayList<TradeDataVO>();
+		for (int i = 0; i < numberOfRecords; i++) {
+			TradeDataVO tradeDataVO = new TradeDataVO();
+			tradeDataVO.setTradeFlow(generateData.generateTradeFlow());
+			tradeDataVO.setMonth(generateData.generateMonth());
+			tradeDataVO.setYear(generateData.generateYear());
+			tradeDataVO.setHsCode(generateData.generateHSCode());
+			tradeDataVO.setProductName(generateData.generateProductName());
+			tradeDataVO.setTotalWeight(generateData.generateTotalWeight());
+			tradeDataVO.setWeightUnits(generateData.generateWeightUnits());
+			tradeDataVO.setPrice(new BigDecimal(generateData.generatePrice()));
+			tradeDataVO.setQuantity(generateData.generateQuantity());
+			tradeDataVOList.add(tradeDataVO);
+		}
+
+		FileWriter fileWriter = null;
+
+		try {
+			System.out.println("**** writeCsvFile started ****");
+			fileWriter = new FileWriter(fileName);
+
+			fileWriter.append("NIKHIL," + new Date().toString());
+
+			// Add a new line separator after the header
+			fileWriter.append(NEW_LINE_SEPARATOR);
+
+			// Write the CSV file header
+			fileWriter.append(FILE_HEADER.toString());
+			fileWriter.append(NEW_LINE_SEPARATOR);
+
+			for (TradeDataVO tradeDataVO : tradeDataVOList) {
+				fileWriter.append(tradeDataVO.getTradeFlow());
+				fileWriter.append(COMMA_DELIMITER);
+				fileWriter.append(String.valueOf(tradeDataVO.getMonth()));
+				fileWriter.append(COMMA_DELIMITER);
+				fileWriter.append(String.valueOf(tradeDataVO.getYear()));
+				fileWriter.append(COMMA_DELIMITER);
+				fileWriter.append(String.valueOf(tradeDataVO.getHsCode()));
+				fileWriter.append(COMMA_DELIMITER);
+				fileWriter.append(tradeDataVO.getProductName());
+				fileWriter.append(COMMA_DELIMITER);
+				fileWriter.append(String.valueOf(tradeDataVO.getTotalWeight()));
+				fileWriter.append(COMMA_DELIMITER);
+				fileWriter.append(tradeDataVO.getWeightUnits());
+				fileWriter.append(COMMA_DELIMITER);
+				fileWriter.append(String.valueOf(tradeDataVO.getPrice()));
+				fileWriter.append(COMMA_DELIMITER);
+				fileWriter.append(String.valueOf(tradeDataVO.getQuantity()));
+				fileWriter.append(NEW_LINE_SEPARATOR);
+			}
+			fileWriter.append("Triler_Count," + tradeDataVOList.size());
+			fileWriter.append(NEW_LINE_SEPARATOR);
+			System.out.println("CSV file was created successfully !!!");
+		} catch (Exception ex) {
+			System.out.println("writeCsvFile failed => " + ex);
+		} finally {
+			try {
+				fileWriter.flush();
+				fileWriter.close();
+			} catch (IOException e) {
+				System.out.println("Error while flushing/closing fileWriter !!!");
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ Csv_RandomData_Generator/src/GenerateData/GenerateData.java
@@ -0,0 +1,137 @@
+package GenerateData;
+
+import java.util.Random;
+
+public class GenerateData {
+
+	/* Column 1 Random Generator */
+	protected String generateTradeFlow() {
+		String tradeFlow = null;
+		try {
+			System.out.println("**** generateTradeFlow Started ****");
+			Random random = new Random();
+			int index = random.nextInt(Utilities.TRADE_FLOW.length);
+			tradeFlow = Utilities.TRADE_FLOW[index];
+			return tradeFlow;
+		} catch (Exception e) {
+			System.out.println("generateTradeFlow Failed ==> " + e);
+			return tradeFlow;
+		}
+	}
+
+	/* Column 2 Random Generator */
+	protected int generateMonth() {
+		int month = 0;
+		try {
+			System.out.println("**** generateMonth Started ****");
+			Random random = new Random();
+			month = random.nextInt(Utilities.MAXIMUM_MONTH - Utilities.MINIMUM_MONTH + 1) + Utilities.MINIMUM_MONTH;
+			return month;
+		} catch (Exception e) {
+			System.out.println("generateMonth Failed ==> " + e);
+			return 0;
+		}
+	}
+
+	/* Column 3 Random Generator */
+	protected int generateYear() {
+		int year;
+		try {
+			System.out.println("**** generateYear Started ****");
+			Random random = new Random();
+			year = random.nextInt(Utilities.MINIMUM_YEAR - Utilities.MINIMUM_YEAR + 1) + Utilities.MINIMUM_YEAR;
+			return year;
+		} catch (Exception e) {
+			System.out.println("generateYear Failed ==> " + e);
+			return 0;
+		}
+	}
+
+	/* Column 4 Random Generator */
+	protected long generateHSCode() {
+		long hsCode;
+		try {
+			System.out.println("**** generateHSCode Started ****");
+			hsCode = (long) Math.floor(Math.random() * 9_000_000_000L) + 1_000_000_000L;
+			return hsCode;
+		} catch (Exception e) {
+			System.out.println("generateHSCode Failed ==> " + e);
+			return 0;
+		}
+	}
+
+	/* Column 5 Random Generator */
+	protected String generateProductName() {
+		String productName = null;
+		try {
+			System.out.println("**** generateProductName Started ****");
+			Random random = new Random();
+			int index = random.nextInt(Utilities.PRODUCTS.length);
+			productName = Utilities.PRODUCTS[index];
+			return productName;
+		} catch (Exception e) {
+			System.out.println("generateProductName Failed ==> " + e);
+			return productName;
+		}
+	}
+
+	/* Column 6 Random Generator */
+	protected int generateTotalWeight() {
+		int totalWeight = 0;
+		try {
+			System.out.println("**** generateTotalWeight Started ****");
+			Random random = new Random();
+			totalWeight = random.nextInt(Utilities.MAXIMUM_WEIGHT - Utilities.MINIMUM_WEIGHT + 1)
+					+ Utilities.MINIMUM_WEIGHT;
+			return totalWeight;
+		} catch (Exception e) {
+			System.out.println("generateTotalWeight Failed ==> " + e);
+			return 0;
+		}
+	}
+
+	/* Column 7 Random Generator */
+	protected String generateWeightUnits() {
+		String weightUnit = null;
+		try {
+			System.out.println("**** generateWeightUnits Started ****");
+			Random random = new Random();
+			int index = random.nextInt(Utilities.WEIGHT_UNITS.length);
+			weightUnit = Utilities.WEIGHT_UNITS[index];
+			return weightUnit;
+		} catch (Exception e) {
+			System.out.println("generateWeightUnits Failed ==> " + e);
+			return weightUnit;
+		}
+	}
+
+	/* Column 8 Random Generator */
+	protected int generatePrice() {
+		int price = 0;
+		try {
+			System.out.println("**** generatePrice Started ****");
+			Random random = new Random();
+			price = random.nextInt(Utilities.MAXIMUM_PRICE - Utilities.MINIMUM_PRICE + 1) + Utilities.MINIMUM_PRICE;
+			return price;
+		} catch (Exception e) {
+			System.out.println("generatePrice Failed ==> " + e);
+			return 0;
+		}
+	}
+
+	/* Column 9 Random Generator */
+	protected int generateQuantity() {
+		int quantity = 0;
+		try {
+			System.out.println("**** generateQuantity Started ****");
+			Random random = new Random();
+			quantity = random.nextInt(Utilities.MAXIMUM_QUANTITY - Utilities.MINIMUM_QUANTITY + 1)
+					+ Utilities.MINIMUM_QUANTITY;
+			return quantity;
+		} catch (Exception e) {
+			System.out.println("generateQuantity Failed ==> " + e);
+			return 0;
+		}
+	}
+
+}
